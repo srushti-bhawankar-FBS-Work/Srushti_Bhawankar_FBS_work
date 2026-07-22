574fe72c5a3d7ebe40cdd7d78af30a56ea5c35a6
srushti-bhawankar-FBS-Work <srushtibhawankar145@gmail.com>
2026-07-23 00:25:29 +0530

    Update que 1.py

--- Core Python/Assignments/assignment 3/que 1.py
+++ Core Python/Assignments/assignment 3/que 1.py
@@ -1,1 +1,6 @@
 #1. Write a program to check if the given number is positive or negative.
+num=int(input('Enter the number:'))
+if(num>0):
+    print('The number is positive.')
+else:
+    print('The number is Negative')
