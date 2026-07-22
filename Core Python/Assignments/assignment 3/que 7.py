6e6dcd02494f7d901bc845ee2f8083b2a87d4129
srushti-bhawankar-FBS-Work <srushtibhawankar145@gmail.com>
2026-07-23 00:30:15 +0530

    Update que 7.py

--- Core Python/Assignments/assignment 3/que 7.py
+++ Core Python/Assignments/assignment 3/que 7.py
@@ -1,1 +1,8 @@
 #7. Write a program to check if user has entered correct userid and password.
+user_id=(input("Enter the user_id:"))
+password=(input("Enter the password:"))
+
+if user_id=='Admin' and password=='1234':
+    print('Login succesfully')
+else:
+    print('The credentials does not match')
