c7c89ebc4038cd86ef73da97c2785582cd186f95
srushti-bhawankar-FBS-Work <srushtibhawankar145@gmail.com>
2026-07-23 00:29:06 +0530

    Update que 4.py

--- Core Python/Assignments/assignment 3/que 4.py
+++ Core Python/Assignments/assignment 3/que 4.py
@@ -1,1 +1,9 @@
 #4. Write a program to input all sides of a triangle and check whether triangle is valid or not.
+a=float(input("Enter the side1:"))
+b=float(input("Enter the side2:"))
+c=float(input("Enter the side3:"))
+
+if (a+b>c) and (a+c>b) and (b+c>a):
+    print("The triangle is valid")
+else:
+    print("The triangle is not valid")
