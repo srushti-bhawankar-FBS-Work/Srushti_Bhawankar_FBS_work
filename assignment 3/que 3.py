81f3446aaa20a6629381ffd1e20943abb4fbf149
srushti-bhawankar-FBS-Work <srushtibhawankar145@gmail.com>
2026-07-23 20:40:47 +0530

    Delete assignment 3 directory

--- assignment 3/que 3.py
+++
@@ -1,9 +0,0 @@
-#3. Write a program to input angles of a triangle and check whether triangle is valid or not.
-a=int(input("Enter the angle1:"))
-b=int(input("Enter the angle2:"))
-c=int(input("Enter the angle3:"))
-
-if a+b+c==180:
-    print('The triangle is valid')
-else:
-    print('The triangle is not valid')
--- assignment 3/que 6.py
+++
@@ -1,1 +0,0 @@
-#6. Write a program to calculate profit or loss.
