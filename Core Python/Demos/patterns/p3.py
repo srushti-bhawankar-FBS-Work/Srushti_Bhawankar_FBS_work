440aa825d978211b2a0c29054f152dd8f80f100c
srushti-bhawankar-FBS-Work <srushtibhawankar145@gmail.com>
2026-07-23 20:42:08 +0530

    Add files via upload

---
+++ Core Python/Demos/patterns/p3.py
@@ -0,0 +1,9 @@
+for i in range(1,6):
+    for j in range(1,6):
+        print(chr(64+i),end='')
+    print()
+    
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(chr(42),end='')
+    print()
